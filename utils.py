06acae01cf5c9c817b52f080b12728d5cb001f36
Aaditya Bajgain <adityabajgain@gmail.com>
2026-01-23 02:37:01 -0800

    implemented logistic regression with regularization

--- utils.py
+++ utils.py
@@ -5,4 +5,23 @@ def load_data(filename):
     y = data[:,2]
     return X,y
 
+def sigmoid(z):
+    return 1/(1 + np.exp(-z))
 
+
+
+def map_feature(x1, x2, degree = 6):
+    x1 = np.atleast_1d(x1)
+    x2 = np.atleast_1d(x2)
+
+
+    out = []
+
+    for i in range(1, degree + 1):
+        for j in range(i+1):
+            out.append((x1**(i-j) * (x2**j)))
+    return np.stack(out, axis = 1)
+
+
+
+        
